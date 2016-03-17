dce945832ffeaadaa5b683a1041fdc834ba5c096
Taiki Yoshikawa <tyoshikawa1106@gmail.com>
2016-03-17 10:38:56 +0900

    commit

--- public/js/jsforce_config.js
+++ public/js/jsforce_config.js
@@ -1,7 +1,14 @@
 console.log(location.host);
-jsforce.browser.init({
-  clientId: '3MVG9I1kFE5Iul2A_s3cQz8y7gXDok4z8Y0zaGRdTEmtj0_CLzKz5Yerankgp0UqVLy7mqPQw5Kf9f6pAni.r',
-  redirectUri: 'http://localhost:8080/',
-  proxyUrl: 'http://localhost:3123/proxy/'
-});
+if (location.host == 'localhost:8080') {
+    jsforce.browser.init({
+      clientId: '3MVG9I1kFE5Iul2A_s3cQz8y7gXDok4z8Y0zaGRdTEmtj0_CLzKz5Yerankgp0UqVLy7mqPQw5Kf9f6pAni.r',
+      redirectUri: 'http://localhost:8080/',
+      proxyUrl: 'http://localhost:3123/proxy/'
+    });
+} else {
+    jsforce.browser.init({
+      clientId: '3MVG9I1kFE5Iul2A_s3cQz8y7gXDok4z8Y0zaGRdTEmtj0_CLzKz5Yerankgp0UqVLy7mqPQw5Kf9f6pAni.r',
+      redirectUri: 'tyoshikawa1106-jsforce-starter/'
+    });
+}
 var conn = jsforce.browser.connection;
