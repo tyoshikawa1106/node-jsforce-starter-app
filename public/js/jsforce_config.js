54a1c859a9d0de2ba9e16f4806fef66ad5338f13
Taiki Yoshikawa <tyoshikawa1106@gmail.com>
2016-03-17 10:35:57 +0900

    commit

--- public/js/jsforce_config.js
+++ public/js/jsforce_config.js
@@ -1,3 +1,4 @@
+console.log(location.host);
 jsforce.browser.init({
   clientId: '3MVG9I1kFE5Iul2A_s3cQz8y7gXDok4z8Y0zaGRdTEmtj0_CLzKz5Yerankgp0UqVLy7mqPQw5Kf9f6pAni.r',
   redirectUri: 'http://localhost:8080/',
