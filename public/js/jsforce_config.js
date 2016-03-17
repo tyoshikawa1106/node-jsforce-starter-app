89391b42b4b0c015a2b471d1435861ed3152957f
Taiki Yoshikawa <tyoshikawa1106@gmail.com>
2016-03-17 10:43:59 +0900

    commit

--- public/js/jsforce_config.js
+++ public/js/jsforce_config.js
@@ -8,7 +8,7 @@ if (location.host == 'localhost:8080') {
 } else {
     jsforce.browser.init({
       clientId: '3MVG9I1kFE5Iul2A_s3cQz8y7gXDok4z8Y0zaGRdTEmtj0_CLzKz5Yerankgp0UqVLy7mqPQw5Kf9f6pAni.r',
-      redirectUri: 'tyoshikawa1106-jsforce-starter/'
+      redirectUri: 'https://tyoshikawa1106-jsforce-starter.herokuapp.com/'
     });
 }
 var conn = jsforce.browser.connection;
