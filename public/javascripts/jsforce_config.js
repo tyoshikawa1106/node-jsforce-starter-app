a48a0474ddae3d0bb0c36bcd759310cf39611614
Taiki Yoshikawa <tyoshikawa1106@gmail.com>
2016-04-18 02:04:03 +0900

    commit

--- public/javascripts/jsforce_config.js
+++ public/javascripts/jsforce_config.js
@@ -1,14 +1,18 @@
-console.log(location.host);
-if (location.host == 'localhost:8080') {
-    jsforce.browser.init({
-      clientId: '<接続アプリケーションコンシューマキー>',
-      redirectUri: 'http://localhost:8080/',
-      proxyUrl: 'http://localhost:3123/proxy/'
-    });
-} else {
-    jsforce.browser.init({
-      clientId: '<接続アプリケーションコンシューマキー>',
-      redirectUri: '<Your Apps URL>'
-    });
+if (!conn) {
+    console.log(location.host);
+    if (location.host == 'localhost:8080') {
+      jsforce.browser.init({
+        clientId: '<your clientId>',
+        redirectUri: 'http://localhost:8080/',
+        proxyUrl: 'http://localhost:3123/proxy/'
+      });
+    } else {
+      jsforce.browser.init({
+        clientId: '<your clientId>',
+        redirectUri: 'https://<your heroku app>.herokuapp.com/',
+        proxyUrl: 'https://<your heroku app>.herokuapp.com/proxy/'
+      });
+    }
+
+    var conn = jsforce.browser.connection;
 }
-var conn = jsforce.browser.connection;
